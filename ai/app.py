7b797e91b3c32320b85670e4702a847b5918c0a4
Osman Temel <temelosman778@gmail.com>
2024-03-02 00:40:29 +0300

    predict adde .

--- ai/app.py
+++ ai/app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model import AiEgitimSonuc
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
 app = Flask(__name__)
 CORS(app)
 
@@ -21,35 +24,33 @@ def receive_data():
         return jsonify({'error': f'Hata: {str(e)}'}), 500
 
 def yapayZeka(secilen_ozellikler, secilen_degerler, data_path):
+    secilen_degerler = pd.DataFrame(secilen_degerler)
+
+        
     ai_egitim_sonucu = AiEgitimSonuc(data_path,secilen_ozellikler, secilen_degerler)
-    ai_egitim_sonucu.model_LR()
-    ai_egitim_sonucu.model_DT()
-    ai_egitim_sonucu.model_RF()
-    ai_egitim_sonucu.model_MLP()
-    ai_egitim_sonucu.model_GNB()
-    ai_egitim_sonucu.model_SVM()
+    ai_egitim_sonucu.model_LR(secilen_degerler)
+    ai_egitim_sonucu.model_DT(secilen_degerler)
+    ai_egitim_sonucu.model_RF(secilen_degerler)
+    ai_egitim_sonucu.model_MLP(secilen_degerler)
+    ai_egitim_sonucu.model_GNB(secilen_degerler)
+    ai_egitim_sonucu.model_SVM(secilen_degerler)
 
     en_iyi_model, en_iyi_model_accuracy = ai_egitim_sonucu.en_iyi_model_ve_accuracy()
-    
-    
+
     print(f"En iyi model: {en_iyi_model}, Accuracy: {en_iyi_model_accuracy}")
     
     if en_iyi_model == "model_LR":
-        secilen_degerler  = pd.DataFrame(secilen_degerler)
-        print(secilen_degerler)
+        ai_egitim_sonucu.model_LR(secilen_degerler)
     elif en_iyi_model == "model_DT":
-        print(secilen_degerler)
-        
+        ai_egitim_sonucu.model_DT(secilen_degerler)
     elif en_iyi_model == "model_RF":
-        print(secilen_degerler)
-
+        ai_egitim_sonucu.model_RF(secilen_degerler)
     elif en_iyi_model == "model_MLP":
-        print(secilen_degerler)
-
+        ai_egitim_sonucu.model_MLP(secilen_degerler)
     elif en_iyi_model == "model_GNB":
-        print(secilen_degerler)
+        ai_egitim_sonucu.model_GNB(secilen_degerler)
     elif en_iyi_model == "model_SVM":
-        print(secilen_degerler)
+        ai_egitim_sonucu.model_SVM(secilen_degerler)  
     else:
         print("ye beni osman")
         
--- ai/model.py
+++ ai/model.py
@@ -21,35 +21,57 @@ class AiEgitimSonuc:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
 
 
-    def _model_egit(self, model, model_ad):
+    def _model_egit(self, model, model_ad,secilen_degerler):
+        
+        secilen_degerler_df = pd.DataFrame(secilen_degerler)    
+        print("secilen_degerler_df",secilen_degerler_df.shape) 
+        secilen_degerler_df=secilen_degerler_df.T  
+        # print("secilen_degerler_df",secilen_degerler_df) 
+        secilen_degerler_df = secilen_degerler_df.fillna(0)
+        current_rows, current_cols = secilen_degerler_df.shape
+        target_rows = 3008
+        target_cols = current_cols
+        if target_rows > current_rows:
+            num_rows_to_add = target_rows - current_rows
+            extension_df = pd.DataFrame(0, index=range(num_rows_to_add), columns=secilen_degerler_df.columns)
+            secilen_degerler_df = pd.concat([secilen_degerler_df, extension_df])
+        print("secilen_degerler_df: ",secilen_degerler_df.shape)       
+        
+        secilen_degerler_df.columns = self.X_test.columns
+        secilen_degerler_df.index = self.X_test.index
+        print(secilen_degerler_df)
+
+        
         model.fit(self.X_train, self.y_train)
         y_pred = model.predict(self.X_test)
+        print( "X_train" ,self.X_train.shape)
+        print("X_test",self.X_test.shape)
         accuracy = accuracy_score(self.y_test, y_pred)
         self.TumSonuclar[model_ad] = accuracy
 
-    def model_LR(self):
+    def model_LR(self,secilen_degerler):
         model_LR = LogisticRegression()
-        self._model_egit(model_LR, "model_LR")
+        self._model_egit(model_LR, "model_LR",secilen_degerler)
 
-    def model_SVM(self):
+    def model_SVM(self,secilen_degerler):
         model_SVM = SVC()
-        self._model_egit(model_SVM, "model_SVM")
+        self._model_egit(model_SVM, "model_SVM",secilen_degerler)
 
-    def model_DT(self):
+    def model_DT(self,secilen_degerler):
         model_DT = DecisionTreeClassifier()
-        self._model_egit(model_DT, "model_DT")
+        self._model_egit(model_DT, "model_DT",secilen_degerler)
 
-    def model_RF(self):
+    def model_RF(self,secilen_degerler):
         model_RF = RandomForestClassifier()
-        self._model_egit(model_RF, "model_RF")
+        self._model_egit(model_RF, "model_RF",secilen_degerler)
 
-    def model_MLP(self):
+    def model_MLP(self,secilen_degerler):
         model_MLP = MLPClassifier()
-        self._model_egit(model_MLP, "model_MLP")
+        self._model_egit(model_MLP, "model_MLP",secilen_degerler)
 
-    def model_GNB(self):
+    def model_GNB(self,secilen_degerler):
         model_GNB = GaussianNB()
-        self._model_egit(model_GNB, "model_GNB")
+        self._model_egit(model_GNB, "model_GNB",secilen_degerler)
 
     def en_iyi_model_ve_accuracy(self):
         en_iyi_model_ad = max(self.TumSonuclar, key=self.TumSonuclar.get)
